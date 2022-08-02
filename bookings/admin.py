b5e1ee7b7e8f25315b0df1d197967589fd6c4808
marktodman <drmtodman@gmail.com>
2022-08-02 20:20:33 +0000

    feat: Create data models for Routes, Trips and Passengers

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Route, Trip, Passenger
 
-# Register your models here.
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+
+    list_display = ('route_name',)
+
+
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name')
+    search_fields = ['last_name']
+
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+
+    list_display = ('trip_name', 'route_name', 'trip_date')
+
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+CABIN_TYPE = ((0, "Shared"), (1, "Private"))
+CREW_OPTION = ((0, "Passenger Only"), (1, "Prepared to Crew"))
+SAILING_EXPERIENCE = ((0, "None"), (1, "Some"), (2, "Lots"))
+
+
+class Route(models.Model):
+    route_name = models.CharField('Route Name', max_length=200, null=False, blank=False)
+    description = models.TextField('Description', null=False, blank=False)
+    duration = models.IntegerField('Duration (days)', null=False, blank=False)
+    distance = models.IntegerField('Distance (miles)', null=False, blank=False)
+    featured_image = CloudinaryField('Image', default='placeholder')
+
+    class Meta:
+        ordering = ['route_name']
+
+    def __str__(self):
+        return self.route_name
+
+
+class Passenger(models.Model):
+    first_name = models.CharField('First Name', max_length=80, null=False, blank=False)
+    last_name = models.CharField('Last Name', max_length=80,null=False, blank=False)
+    email = models.EmailField('Email', max_length=200, blank=False)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Trip(models.Model):
+    trip_name = models.CharField('Trip Name', max_length=200, blank=False)
+    trip_date = models.DateField('Trip Date')
+    route_name = models.ForeignKey(Route, on_delete=models.CASCADE, blank=True, null=True)
+    description = models.TextField(null=False, blank=False)
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank=True, null=True)
+    number_passengers = models.IntegerField('Number of Passengers')
+    cabin_type = models.IntegerField('Cabin Type', choices=CABIN_TYPE, default=0)
+    crew_option = models.IntegerField('Option to Crew', choices=CREW_OPTION, default=0)
+    sailing_exp = models.IntegerField('Previous Sailing Experience', choices=SAILING_EXPERIENCE, default=0)
+    featured_image = CloudinaryField('Image', default='placeholder')
+    interest = models.ManyToManyField(Passenger, related_name='trip_interest', blank=True)
+
+    class Meta:
+        ordering = ['trip_date']
+
+    def __str__(self):
+        return self.trip_name
+
+    def expressions_of_interest(self):
+        return self.interest.count()
