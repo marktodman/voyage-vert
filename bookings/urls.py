c27455ff6e706f65def3780bc7f0839f00244e75
marktodman <drmtodman@gmail.com>
2022-08-09 11:07:46 +0000

    style: Set site wide style theme. Style nav and home page

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('routes', views.RouteList.as_view(), name='routes'),
     path('routes_trips/<route_id>', views.RouteTrips.as_view(), name='route-trips'),
-    # path('route_trips/<route_id>', views.route_trips, name='route-trips'),
 ]
