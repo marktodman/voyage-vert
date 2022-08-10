981d8c46038c15bfe6f1c50816ba90668b36d772
marktodman <drmtodman@gmail.com>
2022-08-10 10:16:17 +0000

    style: Style trip cards that display the trips for each route available for booking

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('routes', views.RouteList.as_view(), name='routes'),
-    path('routes_trips/<route_id>', views.RouteTrips.as_view(), name='route-trips'),
+    path('route_trips/<route_id>', views.RouteTrips.as_view(), name='route-trips'),
 ]
